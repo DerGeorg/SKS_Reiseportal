87a52c398fe4f491c063d916f70966bcb86dcfe6
Georg Reisinger <georg@dergeorg.at>
2022-01-03 18:07:54 +0100

    Add Config Server

---
+++ configserver/src/test/java/at/technikum/wien/if20b206/if20b211/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.wien.if20b206.if20b211.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
